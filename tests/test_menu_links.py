ac4e38660e1747bd2d62ff1b51ca57d814f693f9
Nazariy <51sttestinrror@gmail.com>
2024-04-01 16:20:44 +0300

    rewrite test_links case

--- tests/test_menu_links.py
+++ tests/test_menu_links.py
@@ -11,19 +11,19 @@ logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
 class TestLinks:
 
     @pytest.fixture(autouse=True)
-    def setup_data(self, load_test_data):
+    def setup_data(self, load_test_data, browser: Browser):
+        self.base_url = load_test_data['base_url']
         self.about_link = load_test_data['links']['about_link']
         self.standard_user_data = load_test_data['test_users']['standard']
-        self.locked_user_data = load_test_data['test_users']['locked']
+        self.page = browser.new_page()
 
-    def test_about_link(self, browser: Browser):
-        page = browser.new_page()
-        login_page = LoginPage(page)
-        product_page = InventoryPage(page)
-        login_page.navigate(login_page.URL)
+    def test_about_link(self):
+        login_page = LoginPage(self.page, self.base_url)
+        login_page.navigate(login_page.url)
         login_page.login_user(self.standard_user_data['username'], self.standard_user_data['password'])
+        product_page = InventoryPage(self.page, self.base_url)
         expect(product_page.header).to_be_visible()
         product_page.select_menu_option(product_page.about_link)
         product_page.wait_for_url(self.about_link)
-        assert page.url == self.about_link
-        assert page.title() == "Sauce Labs: Cross Browser Testing, Selenium Testing & Mobile Testing"
+        assert self.page.url == self.about_link
+        assert self.page.title() == "Sauce Labs: Cross Browser Testing, Selenium Testing & Mobile Testing"
